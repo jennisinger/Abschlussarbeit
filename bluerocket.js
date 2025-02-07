c66a593f8da95632a34cd9ae51804975fa77424d
jennisinger <jsi47883@stud.hs-furtwangen.de>
2025-02-07 18:18:38 +0100

    neu

--- bluerocket.js
+++ bluerocket.js
@@ -1,7 +1,7 @@
 "use strict";
 var Rakete2;
 (function (Rakete2) {
-    class Rocket1 {
+    class Rocket2 {
         // Eigenschaften für den Körper, Kopf und Zündschnur der Rakete
         bodyWidth;
         bodyHeight;
@@ -16,7 +16,7 @@ var Rakete2;
         // Methode, um die Rakete zu zeichnen
         drawRocket(ctx, x, y) {
             // Körper der Rakete (Rechteck)
-            ctx.fillStyle = "red";
+            ctx.fillStyle = "blue";
             ctx.fillRect(x - this.bodyWidth / 2, y - this.bodyHeight, this.bodyWidth, this.bodyHeight);
             // Kopf der Rakete (Dreieck)
             ctx.beginPath();
@@ -24,12 +24,12 @@ var Rakete2;
             ctx.lineTo(x - this.bodyWidth / 2, y - this.bodyHeight); // Linke untere Ecke
             ctx.lineTo(x + this.bodyWidth / 2, y - this.bodyHeight); // Rechte untere Ecke
             ctx.closePath();
-            ctx.fillStyle = "red";
+            ctx.fillStyle = "blue";
             ctx.fill();
             ctx.beginPath();
             ctx.moveTo(x, y); // Untere Mitte des Raketen-Körpers
             ctx.lineTo(x, y + this.fuseLength); // Ende der Zündschnur
-            ctx.strokeStyle = "red";
+            ctx.strokeStyle = "blue";
             ctx.lineWidth = 2;
             ctx.stroke();
         }
@@ -38,8 +38,8 @@ var Rakete2;
     const canvas = document.getElementById("myCanvas");
     const ctx = canvas.getContext("2d");
     if (ctx) {
-        const rocket = new Rocket1(50, 100, 30, 50); // Beispiel mit Breite 50, Höhe 100, Kopfhöhe 30 und Zündschnurlänge 50
+        const rocket = new Rocket2(50, 100, 30, 50); // Beispiel mit Breite 50, Höhe 100, Kopfhöhe 30 und Zündschnurlänge 50
         rocket.drawRocket(ctx, canvas.width / 2, canvas.height / 2);
     }
 })(Rakete2 || (Rakete2 = {}));
-//# sourceMappingURL=redrocket.js.map
+//# sourceMappingURL=bluerocket.js.map
--- bluerocket.ts
+++ bluerocket.ts
@@ -1,6 +1,6 @@
 namespace Rakete2
 {
-class Rocket1 {
+class Rocket2 {
     // Eigenschaften für den Körper, Kopf und Zündschnur der Rakete
     bodyWidth: number;
     bodyHeight: number;
@@ -17,7 +17,7 @@ class Rocket1 {
     // Methode, um die Rakete zu zeichnen
     drawRocket(ctx: CanvasRenderingContext2D, x: number, y: number): void {
       // Körper der Rakete (Rechteck)
-      ctx.fillStyle = "red";
+      ctx.fillStyle = "blue";
       ctx.fillRect(x - this.bodyWidth / 2, y - this.bodyHeight, this.bodyWidth, this.bodyHeight);
   
       // Kopf der Rakete (Dreieck)
@@ -26,13 +26,13 @@ class Rocket1 {
       ctx.lineTo(x - this.bodyWidth / 2, y - this.bodyHeight); // Linke untere Ecke
       ctx.lineTo(x + this.bodyWidth / 2, y - this.bodyHeight); // Rechte untere Ecke
       ctx.closePath();
-      ctx.fillStyle = "red";
+      ctx.fillStyle = "blue";
       ctx.fill();
   
       ctx.beginPath();
       ctx.moveTo(x, y); // Untere Mitte des Raketen-Körpers
       ctx.lineTo(x, y + this.fuseLength); // Ende der Zündschnur
-      ctx.strokeStyle = "red";
+      ctx.strokeStyle = "blue";
       ctx.lineWidth = 2;
       ctx.stroke();
     }
@@ -43,7 +43,7 @@ class Rocket1 {
   const ctx = canvas.getContext("2d");
   
   if (ctx) {
-    const rocket = new Rocket1(50, 100, 30, 50); // Beispiel mit Breite 50, Höhe 100, Kopfhöhe 30 und Zündschnurlänge 50
+    const rocket = new Rocket2 (50, 100, 30, 50); // Beispiel mit Breite 50, Höhe 100, Kopfhöhe 30 und Zündschnurlänge 50
     rocket.drawRocket(ctx, canvas.width / 2, canvas.height / 2);
   }
-} 
+}
